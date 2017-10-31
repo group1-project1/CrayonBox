f9aa67a89fd8ec7caa4e85472320af9e76ed2a60
fjnw <faizanjamal2018@u.northwestern.edu>
2017-10-30 20:58:52 -0500

    Changed Id's in modal. Tied weather call to Id in model.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,28 +1,19 @@
 var apiKey = "3197191ce64f4094b7b04205172410";
 var weatherUrl = 'http://api.apixu.com/v1/current.json';
-var weatherIconUrl;
 
-function weatherCall(lat, lon) {
+function weatherCall(lat, lon, date) {
 	$.ajax({
 	   url: weatherUrl,
 	   method: "GET",
 	   data: {key: apiKey, 
 	   		 // search parameter (city, zip, grid coordinates)
-	   		 q: array[i][lat,lon].val().trim(),
+	   		 q: lat + "," + lon,
 	   		 // number of days until event, limited to <=10 days in the future
-	   		 days: array[i]["date"].diff(moment(), 'days') 
+	   		 days: moment(date).diff(moment(), 'days') 
 	   		}
 	}).done(function(response) {
-		console.log(this.url);
-		console.log(response);
-		weatherIconUrl = response.current.condition.icon.substring(2);
+		$('#event-weather').html('<img src=https:"' + response.current.condition.icon + '"/>')
 	}).fail(function(response){
 		console.log(this.url);
 	});
 };
-
-
-// EX: date difference in moment.js
-	// var now = moment(new Date()); //todays date
-	// var end = moment("2017-11-02"); // another date
-	// console.log(end.diff(now, 'days'))
